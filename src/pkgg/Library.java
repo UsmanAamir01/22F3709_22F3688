3670aee36953b29b1407eac89a8f8a4e65f4d413
MuhammadSarim07 <f223688@cfd.nu.edu.pk>
2024-10-05 03:43:38 +0500

    Enhanced Library class with borrow and return functionality #1

--- src/pkgg/Library.java
+++ src/pkgg/Library.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Library {
 	private List<String> books = new ArrayList<>();
+	private List<String> borrowedBooks = new ArrayList<>();
 
 	public void addBook(String bookName) {
 		books.add(bookName);
@@ -12,6 +13,9 @@ public class Library {
 	}
 
 	public void removeBook(String bookName) {
+		if (books.isEmpty()) {
+			System.out.println("No books available in the library to remove.");
+		} else if (books.remove(bookName)) {
 		if (books.remove(bookName)) {
 			System.out.println(bookName + " removed from library.");
 		} else {
@@ -19,8 +23,29 @@ public class Library {
 		}
 	}
 
+	public void borrowBook(String bookName) {
+		if (books.contains(bookName)) {
+			books.remove(bookName);
+			borrowedBooks.add(bookName);
+			System.out.println(bookName + " has been borrowed.");
+		} else {
+			System.out.println(bookName + " is either not available or already borrowed.");
+		}
+	}
+
+	public void returnBook(String bookName) {
+		if (borrowedBooks.contains(bookName)) {
+			borrowedBooks.remove(bookName);
+			books.add(bookName);
+			System.out.println(bookName + " has been returned.");
+		} else {
+			System.out.println(bookName + " was not borrowed from this library.");
+		}
+	}
+
 	public void listBooks() {
-		System.out.println("Books in library: " + books);
+		System.out.println("Available books: " + books);
+		System.out.println("Borrowed books: " + borrowedBooks);
 	}
 
 	public boolean searchBook(String bookName) {
@@ -32,6 +57,13 @@ public class Library {
 			Library myLibrary = new Library();
 			myLibrary.addBook("Harry Potter");
 			myLibrary.addBook("Lord of the Rings");
+
+			myLibrary.borrowBook("Harry Potter");
+			myLibrary.borrowBook("The Hobbit");
+			myLibrary.listBooks();
+
+			myLibrary.returnBook("Harry Potter");
+			myLibrary.returnBook("The Hobbit");
 			myLibrary.listBooks();
 		}
 	}
